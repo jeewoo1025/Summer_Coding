6e5db6dfdab6a2d817ef8fae650ddbc7aa6f6a85
JEEWOO SUL <39071676+jeewoo1025@users.noreply.github.com>
2021-09-09 16:18:38 +0900

    보석 쇼핑 ver2 성공!

--- August/prog_67258.py
+++ August/prog_67258.py
@@ -1,53 +1,65 @@
-# 구간 체크하는 함수
-def check(start, end, data):  # 시작, 끝, 리스트
-    print(start, '~', end, end='  ')
-    # 크기 < N -> 안됨
-    if len(data[start:end+1]) < N:
-        print("안됨 1")
-        return []
-
-    # set(data) != jewerly -> 안됨
-    if set(data[start:end+1]) != jewerly:
-        print(set(data[start:end+1]), "안됨 2")
-        return []
-
-    # Leaf 노드일 때 -> 됨
-    if start == end:
-        print("리프 노드")
-        return [start, end]
-
-    # 된다면 크기 줄이기
-    mid = len(data) // 2
-    if mid == end:
-        return []
-    print('mid : ',mid)
-
-    if check(start, mid-1, data):
-        print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-        return [start, mid]
-    if check(mid, end, data):
-        print("ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ")
-        return [mid+1, end]
-    print("None")
-    return [start, end]
-
-
 def solution(gems):
-    global jewerly, N  # 전역변수
     jewerly = set(gems)
-    N = len(jewerly)
+    MIN, MAX = len(jewerly), len(gems)
 
-    answer = check(0, len(gems)-1, gems)
-    return [answer[0]+1, answer[1]+1]
+    # 크기 MIN ~ MAX까지 조회
+    for size in range(MIN, MAX+1):
+        start = 0
+        while start < len(gems):
+            end = start+size-1
+            if end >= len(gems):
+                break
+
+            # 만약 보석 다 포함하면 break
+            #if isOK(start, end, gems):
+            #   return [start+1, end+1]
+            start += 1
+
+    return [1, len(gems)]
 
 
-tmp = [
-    ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"],
-    ["XYZ", "XYZ", "XYZ"],
-    ["AA", "AB", "AC", "AA", "AC"],
-    ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
-]
+def check(l, r, size):
+    if l<=0 or l>=size or r<=0 or r>=size:
+        return False
+    return True
 
-for t in tmp:
-    print(t)
-    print('정답 :', solution(t),end='\n\n')
+
+# 투포인터 문제
+def solution2(gems):
+    answer = []
+    shortest = len(gems)+1
+
+    start,end = 0,0
+
+    jewerly_type = len(set(gems))  # 보석의 총 종류수
+    jewerly = {}  # 현재 구간에 포함되어 있는 보석 종류
+
+    while end < len(gems):
+        # 현재 보석 종류 update
+        if gems[end] not in jewerly:
+            jewerly[gems[end]] = 1
+        else:
+            jewerly[gems[end]] += 1
+
+        end += 1
+
+        # 현재 범위 안 보석종류 = 총 종류수
+        if len(jewerly) == jewerly_type:
+            # start가 end보다 같을 때까지 증가
+            while start < end:
+                if jewerly[gems[start]] > 1:  # 2개 이상 있는 거
+                    jewerly[gems[start]] -= 1
+                    start += 1
+
+                # 기존 구간 최단거리 > 현재 구간거리
+                elif shortest > (end-start):
+                    shortest = end-start
+                    answer = [start+1, end]
+                    break
+                else:
+                    break
+    return answer
+
+
+tmp = ["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+print(solution2(tmp))
---
+++ August/temp.py
@@ -0,0 +1,9 @@
+
+tmp = {
+    'r':0,
+    'rw':1,
+    'rrr':2
+}
+
+tmp = [x for x in tmp]
+print(tmp)
