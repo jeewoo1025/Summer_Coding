39d6f309777d84e084818c95462507148b5e075d
JEEWOO SUL <39071676+jeewoo1025@users.noreply.github.com>
2021-11-19 00:01:40 +0900

    백준 14938번 코드 수정

--- November/bj_14938.py
+++ November/bj_14938.py
@@ -1,7 +1,6 @@
 # 백준 14398번 서강그라운드
 import sys
 
-
 # 입력
 INF = sys.maxsize
 
@@ -12,7 +11,7 @@ items.insert(0, -1)
 d = [[INF]*(N+1) for _ in range(N+1)]
 for _ in range(R):
     a,b,l = map(int, input().split())
-    if l <= M:
+    if l <= M: # 간선이 범위 내이여야 함
         d[a][b] = l
         d[b][a] = l
 
@@ -23,7 +22,7 @@ for k in range(1,N+1):
     for i in range(1,N+1):
         for j in range(1,N+1):
             temp = d[i][k] + d[k][j]
-            if temp<d[i][j] and temp<=M:
+            if temp < d[i][j] and temp <= M:
                 d[i][j] = temp
 
 # debug
@@ -31,7 +30,7 @@ answer = 0
 for i in range(1,N+1):
     # 자기자신 + 연결된 노드의 아이템
     val = items[i]
-    val += sum([items[j] for j in range(1,N+1) if j!=i and d[i][j]!=INF])
+    val += sum([items[j] for j in range(1, N+1) if j!=i and d[i][j]!=INF])
     # print(i,'번째 노드 기준 = ', val)
     answer = max(answer, val)
 print(answer)
